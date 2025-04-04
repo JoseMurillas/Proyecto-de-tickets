e75dd6ee1f39db8468a7fd986937f28edd470da0
GustavoVergel <tavovergel@gmail.com>
2025-04-04 10:26:04 -0500

    carpetas cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,34 @@
 import { Routes } from '@angular/router';
-import path from 'node:path';
 
 export const routes: Routes = [
+    
+    {
+        //Ruta del Home separada de los componentes del dashboard
+        path: '',
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent), // Home sin layout
+    },
     {
         path: '',
         loadComponent: () => import('./shared/components/layout/layout.component'),
-        children: [ 
+        children: [
             {
                 path: 'dashboard',
                 loadComponent: () => import('./business/dashboard/dashboard.component'),
             },
             {
                 path: 'profile',
-                loadComponent: () => import('./business/profile/profile.component')
+                loadComponent: () => import('./business/profile/profile.component'),
             },
             {
                 path: 'tables',
                 loadComponent: () => import('./business/tables/tables.component'),
             },
-            {
-                path: '',
-                redirectTo: "dashboard",
-                pathMatch: 'full'
-            }
-           
         ]
     },
     {
-        path: '**',
-        redirectTo: 'dashboard',
+        path: '',
+        redirectTo: '', // Redirige a Home si la ruta no existe
         pathMatch: 'full'
+        
     }
 ];
