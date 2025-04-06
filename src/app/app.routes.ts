b7ca94af306e37c167956c0f5fb0baa5cbc1f25c
Hector Florez <hectorflorez25@gmail.com>
2025-04-06 13:35:03 -0500

    release/conciertos - se crea la pagina principal seccion conciertos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,6 +28,29 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./landing/landing.component').then(m => m.LandingComponent),
+        children: [
+          {
+            path: 'sections/conciertos',
+            loadComponent: () => import('./landing/sections/conciertos-section/conciertos-section.component').then(m => m.ConciertosSectionComponent),
+            pathMatch: 'full'
+          },
+          {
+            path: 'sections/contactanos',
+            loadComponent: () => import('./landing/sections/contactanos-section/contactanos-section.component').then(m => m.ContactanosSectionComponent),
+            pathMatch: 'full'
+          },
+          { path: '', redirectTo: 'sections/conciertos', pathMatch: 'full' },
+          {
+            path: 'login',
+            redirectTo: 'login',
+            pathMatch: 'full'
+          },
+          {
+            path: 'register',
+            redirectTo: 'register',
+            pathMatch: 'full'
+          }
+        ]
     },
     {
       path: '',
